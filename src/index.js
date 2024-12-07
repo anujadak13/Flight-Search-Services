46639ef952e60aa4e98c35d1127add0d8cb44fa8
Anuj Kumar <adakdhuriya3@gmail.com>
2024-12-07 12:49:34 +0530

    database setup done

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const { PORT } = require("./config/serverConfig")
+
+
+const app = express()
+
+
+const SetupAndStartServer = async ()=>{
+
+ app.use(bodyParser.json())
+ app.use(bodyParser.urlencoded({extended:true}))   
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at ${PORT}`)
+})
+
+}
+
+SetupAndStartServer()
